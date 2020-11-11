777dee481b85881d26df95d6f4fb3d2e59dde981
Kumud <kumudsharma246@gmail.com>
2020-11-11 23:50:23 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Main from "./Main";
-import Login from "./Login";
 import Error from "./Error"
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <>
       <Switch>
         <Route exact path = '/' component = {Main} />
-        <Route path = '/login'  component = {Login} />
         <Route component = {Error} />
       </Switch>
     </>
