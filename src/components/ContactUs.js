ec755f62e29a88a2734960243d355f654e0e4c83
Sakshi Tomar <sakshiyukta564@gmail.com>
2021-04-15 00:31:06 +0530

    hello

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -2,16 +2,9 @@ import { Card } from '@material-ui/core'
 import React from 'react'
 import CardContent from '@material-ui/core/CardContent';
 import { Title } from 'react-admin';
-import BackButton from "./BackButton";
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+// import BackButton from "./BackButton";
+// import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 // import insta from "../photos/insta.png";
-import {
-    List,
-    Datagrid,
-    TextField,
-    EmailField,
-    DeleteButton,
-} from 'react-admin'
 import '../css/contact.css'
 import InstagramIcon from '@material-ui/icons/Instagram';
 
@@ -23,8 +16,8 @@ const ContactList = (props) => {
             <CardContent>
                 <h1 className = "email">Email Id</h1>
                 <p>In case of any query, feel free to contact us at</p>
-                <p><a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=sarvsahayak@gmail.com" target="_blank" >  sarvsahayak@gmail.com</a></p>
-                <a href="http://www.instagram.com/sarvsahayak" target="_blank">
+                <p><a href="mailto:sarvsahayak@gmail.com">sarvsahayak@gmail.com</a></p>
+                <a href="http://www.instagram.com/sarvsahayak">
                     <InstagramIcon fontSize = "large"/>
                 </a>
             </CardContent>
